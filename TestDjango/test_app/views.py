8db25c7dfd172b23ea2a6eee2fc06ff531ce2b4e
hijeck11 <pashkevich.anton.v@gmail.com>
2023-10-30 09:06:28 +0300

    Созадние URL-адреосв

---
+++ TestDjango/test_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Главная</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Наш клуб</h1>')
+
+def adress(request):
+    return HttpResponse('<h1>На этой страницу указаны адрес и контакты нашего клуба</h1>')
+
+def help(request):
+    return HttpResponse('<h1>Помощь</h1>')
