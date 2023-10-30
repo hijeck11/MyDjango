dfc90f69fbccc84da333a15d3120ff00df3822d9
hijeck11 <pashkevich.anton.v@gmail.com>
2023-10-30 09:01:37 +0300

    Создание модели

--- TestDjango/test_app/models.py
+++ TestDjango/test_app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-class Persons(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    age = models.DecimalField(max_digits=100)
-    address = models.CharField(max_length=100)
+
+
+
+class Testmodel(models.Model):
+    firstname_test = models.CharField(max_length=100)
+    lasttname_test = models.CharField(max_length=100)
+    description = models.TextField()
+
 
 # class MyModel(models.Model):
 #     field1 = models.CharField(max_length=100)
