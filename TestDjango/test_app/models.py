a07f111786298fb46a41c27e0ccd166d3bd043c4
hijeck11 <pashkevich.anton.v@gmail.com>
2023-11-13 18:30:14 +0300

    Вывод картинок их БД и переход на каждую по клику на неё

--- TestDjango/test_app/models.py
+++ TestDjango/test_app/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
-
-
-
 class Testmodel(models.Model):
     firstname_test = models.CharField(max_length=100)
-    lasttname_test = models.CharField(max_length=100)
+    lastname_test = models.CharField(max_length=100)
     description = models.TextField()
+    model_img = models.ImageField(upload_to='img/')
 
 
-# class MyModel(models.Model):
-#     field1 = models.CharField(max_length=100)
-#     field2 = models.IntegerField()
-#     field3 = models.BooleanField(default=False)
--- TestDjango/test_app/views.py
+++ TestDjango/test_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Testmodel
+from django.shortcuts import render, get_object_or_404
+
+
 
 def home(request):
     return HttpResponse('<h1>Главная</h1>')
@@ -15,3 +19,11 @@ def help(request):
 
 def flowers(request):
     return render(request, 'test_app/flowers.html')
+
+def index(request):
+    num_img = Testmodel.objects.all()
+    return render(request, 'test_app/index.html', context={'num_img': num_img})
+
+def image_detail(request, image_id):
+    image = get_object_or_404(Testmodel, pk=image_id)
+    return render(request, 'test_app/image_detail.html', {'image': image})
