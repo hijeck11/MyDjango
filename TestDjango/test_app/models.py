2735ceac9ba8ff56e69c5baf61d94192d297c72e
hijeck11 <pashkevich.anton.v@gmail.com>
2023-11-15 18:33:01 +0300

    Добавил переходы в браузере.

--- TestDjango/test_app/models.py
+++ TestDjango/test_app/models.py
@@ -12,6 +12,8 @@ class Library(models.Model):
 
     def __str__(self):
         return self.name
+    def get_authors(self):
+        return Author.objects.filter(libraries=self)
 
 class Author(models.Model):
     first_name_author = models.CharField(max_length=100)
@@ -19,12 +21,22 @@ class Author(models.Model):
     birthday = models.DateField()
     libraries = models.ManyToManyField(Library, related_name='authors')
 
+#todo related_name = 'authors': Это аргумент, который устанавливает
+# имя обратного связанного поля для Library. Когда вы используете related_name, Django создает
+# обратное поле в модели Library, которое позволяет вам обращаться к связанным авторам через объекты библиотек.
+
+
     def __str__(self):
         return self.second_name_author
+
+    def get_books_in_library(self, library):
+        return Book.objects.filter(author=self, author__libraries=library)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+#todo models.CASCADE: автоматически удаляет строку из зависимой таблицы, если удаляется связанная строка из главной таблицы
 
     def __str__(self):
         return self.title
