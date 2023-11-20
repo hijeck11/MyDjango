8e32aaa0965df5aa921671fd813e4da57786513a
hijeck11 <pashkevich.anton.v@gmail.com>
2023-11-20 03:18:39 +0300

    Добавил на главную информацию по книгам, forms.py для добавления новой редактирования либо удаления старой книги.

--- TestDjango/test_app/urls.py
+++ TestDjango/test_app/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from django.urls import path
 from . import views
 from django.conf import settings
-from .views import library_list, AuthorListView, BookListView, library_authors, author_books
-
+from .views import library_list, AuthorListView, BookListView, library_authors, author_books, home
+from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
 	path('', views.home, name='blog-home'),
@@ -20,6 +20,11 @@ urlpatterns = [
 	path('books/', BookListView.as_view(), name='book_list'),
 	path('libraries/<int:library_id>/authors/', library_authors, name='library_authors'),
 	path('libraries/<int:library_id>/authors/<int:author_id>/books/', author_books, name='author_books'),
+	path('', home, name='home'),
+	path('add/', add_book, name='add_book'),
+	path('edit/<int:book_id>/', edit_book, name='edit_book'),
+	path('delete/<int:book_id>/', delete_book, name='delete_book'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
